2d57274f84d54e73cbfb7614a9e0994cb071164d
xiao_wine <xiaowine0@gmail.com>
2022-05-02 17:01:44 +0800

    init

--- app/src/main/java/cn/inksay/xiaowine/hook/app/SystemUI.kt
+++ app/src/main/java/cn/inksay/xiaowine/hook/app/SystemUI.kt
@@ -7,10 +7,9 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.content.res.Configuration
 import android.graphics.Color
-import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.os.Message
+import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -24,6 +23,7 @@ import cn.inksay.xiaowine.utils.XposedOwnSP.config
 import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.hookAfter
 import java.util.*
+import cn.inksay.xiaowine.utils.SystemProperties
 
 @SuppressLint("StaticFieldLeak")
 object SystemUI : BaseHook() {
@@ -31,24 +31,16 @@ object SystemUI : BaseHook() {
     @SuppressLint("StaticFieldLeak") var textView: TextView? = null
     private var context: Context? = null
     private var updateTextHandler: Handler? = null
-    private var waitUpate = false
+    private var waitUpdate = false
+    private var sayText = ""
+    private val inkSayReceiver by lazy { InkSayReceiver() }
 
     @SuppressLint("SetTextI18n") override fun init() {
-        val inkSayReceiver by lazy { InkSayReceiver() }
-        val updateMargins = Handler(Looper.getMainLooper()) { message ->
-            LogUtils.i("${message.obj} ${textView.isNull()}")
-            val layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-            layoutParams.setMargins(0, message.obj as Int, 0, 0)
-            textView!!.layoutParams = layoutParams
-
+        updateTextHandler = Handler(Looper.getMainLooper()) {
+            LogUtils.i("刷新文字")
+            textView?.text = sayText
             true
         }
-        updateTextHandler = Handler(Looper.getMainLooper()) { message: Message ->
-            LogUtils.i("更新文字：${message.data.getString("value")!!}")
-            textView?.text = message.data.getString("value") ?: "大海中的麻子：无处可寻"
-            true
-        }
-        LogUtils.i("初始化")
         if (!Utils.isMiui) {
             LogUtils.i("本设备非MIUI")
             LogUtils.i("本模块仅支持MIUI")
@@ -58,43 +50,57 @@ object SystemUI : BaseHook() {
             return
         }
         findMethod("com.android.systemui.qs.MiuiQSHeaderView") { name == "onFinishInflate" }.hookAfter { methodHookParam ->
-            LogUtils.i("初始化View")
             val viewGroup = methodHookParam.thisObject as ViewGroup
             context = viewGroup.context
-            textView = TextView(context).apply {
-                text = "test" //setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
-                textSize = 15f
-                setTextColor(Color.WHITE)
-//                paint.isFakeBoldText = true
-//                layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT).also { it.setMargins(0, Utils.dp2px(context, 15f), 0, 0) }
-
-            }
-            viewGroup.addView(textView)
-            runCatching { context!!.unregisterReceiver(inkSayReceiver) }
-            context!!.registerReceiver(inkSayReceiver, IntentFilter().apply { addAction("InkSay_Server") })
+            if (SystemProperties[context!!, "ro.build.date.utc"].toInt() >= 1647014400 && !SystemProperties[context!!, "ro.build.version.incremental"].endsWith("DEV") && !SystemProperties[context!!, "ro.build.version.incremental"].endsWith("XM")) return@hookAfter
+            init(context!!, viewGroup)
         }
-
+        findMethod("com.android.systemui.qs.MiuiNotificationHeaderView") { name == "onFinishInflate" }.hookAfter { methodHookParam ->
+            val viewGroup = methodHookParam.thisObject as ViewGroup
+            context = viewGroup.context
+            if (!(SystemProperties[context!!, "ro.build.date.utc"].toInt() >= 1647014400 && !SystemProperties[context!!, "ro.build.version.incremental"].endsWith("DEV") && !SystemProperties[context!!, "ro.build.version.incremental"].endsWith("XM"))) return@hookAfter
+            val bigTimeId = context!!.resources.getIdentifier("big_time", "id", context!!.packageName)
+            val bigTime: TextView = viewGroup.findViewById(bigTimeId)
+            val dateTimeId = context!!.resources.getIdentifier("date_time", "id", context!!.packageName)
+            val dateTime: TextView = viewGroup.findViewById(dateTimeId)
+            viewGroup.removeView(bigTime)
+            viewGroup.removeView(dateTime)
+            init(context!!, viewGroup)
+            viewGroup.addView(bigTime)
+            viewGroup.addView(dateTime)
+        }
         findMethod("com.android.systemui.qs.MiuiQSHeaderView") { name == "updateLayout" }.hookAfter {
-            LogUtils.i("")
-            getDateUpdate()
-            context.isNotNull {
+            if (context.isNotNull() && textView.isNotNull()) {
                 LogUtils.i(context!!.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE)
-                updateMargins.sendMessage(updateMargins.obtainMessage().also {
-                    it.obj = if (context!!.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) 0 else Utils.dp2px(context!!, 15f)
-                })
-
+                textView!!.visibility = if (context!!.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) View.GONE else View.VISIBLE
             }
         }
+        UpdateTask().cancel()
         if (config.getUpdateInterval() != 0) {
-            startTimer(((config.getUpdateInterval() * 1000).toLong()), getUpdateDateTimer())
+            LogUtils.i(config.getUpdateInterval() * 1000)
+            Timer().scheduleAtFixedRate(UpdateTask(), 0, (config.getUpdateInterval() * 1000).toLong())
+            println("其他任务")
         }
     }
 
+    private fun init(context: Context, viewGroup: ViewGroup) {
+        textView = TextView(context).apply {
+            text = sayText
+            //setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
+            textSize = 15f
+            setTextColor(Color.WHITE)
+            layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT).also { it.setMargins(0, Utils.dp2px(context, 15f), 0, 0) }
+        }
+        viewGroup.addView(textView)
+        runCatching { context.unregisterReceiver(inkSayReceiver) }
+        context.registerReceiver(inkSayReceiver, IntentFilter().apply { addAction("InkSay_Server") })
+    }
+
     fun getDateUpdate() {
-        if (!waitUpate) {
+        if (waitUpdate) {
             return
         } else {
-            waitUpate = true
+            waitUpdate = true
         }
         Thread {
             textView.isNull {
@@ -103,43 +109,25 @@ object SystemUI : BaseHook() {
             }
             LogUtils.i("获取一言")
             val request = ActivityUtils.getHttp("https://v1.hitokoto.cn/?encode=text")
-            if (request.isNull()) {
-                LogUtils.i("获取失败")
-            } else {
-                LogUtils.i("发送更新")
-                updateTextHandler!!.sendMessage(updateTextHandler!!.obtainMessage().also {
-                    it.data = Bundle().apply { putString("value", request) }
-                })
-            }
+            sayText = request ?: "大海中的麻子：无处可寻"
+            updateTextHandler!!.sendMessage(updateTextHandler!!.obtainMessage())
+            LogUtils.i(if (request.isNull()) "获取失败" else "发送刷新:${sayText}")
+            waitUpdate = false
         }.start()
     }
 
+    //任务广播
     class InkSayReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
+            LogUtils.i("收到广播，开始刷新")
             getDateUpdate()
         }
     }
 
-
-    private var updateDateTimer: TimerTask? = null
-
-    private fun startTimer(period: Long, timerTask: TimerTask) {
-        val timerQueue: ArrayList<TimerTask> = arrayListOf()
-        timerQueue.forEach { task -> if (task == timerTask) return }
-        timerQueue.add(timerTask)
-        val timer = Timer()
-        timer.schedule(timerTask, 0, period)
-        LogUtils.i("开启 ${timerTask::class} 循环")
-    }
-
-    private fun getUpdateDateTimer(): TimerTask {
-        if (updateDateTimer == null) {
-            updateDateTimer = object : TimerTask() {
-                override fun run() {
-                    getDateUpdate()
-                }
-            }
+    internal class UpdateTask : TimerTask() {
+        override fun run() {
+            getDateUpdate()
         }
-        return updateDateTimer as TimerTask
     }
+
 }
